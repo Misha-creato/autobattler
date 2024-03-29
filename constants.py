7dc61cb3cf4677693ede949172bfc067ebd10bfc
Iri O <oparina.iri@gmail.com>
2024-03-29 19:26:14 +0500

    Created constants file

---
+++ constants.py
@@ -0,0 +1,20 @@
+from characters import (
+    Penguin,
+    Cat,
+    Granny,
+    Teen,
+    Soldier,
+    Stormtrooper,
+    TRex
+)
+
+
+CHARACTER_CLASSES = [
+    Penguin,
+    Cat,
+    Granny,
+    Teen,
+    Soldier,
+    Stormtrooper,
+    TRex
+]
