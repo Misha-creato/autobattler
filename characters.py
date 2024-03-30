63170077fb63ff7f1aaa61a46364b298cf1d2418
Iri O <oparina.iri@gmail.com>
2024-03-30 19:21:38 +0500

    Added name field, use_attack method

--- characters.py
+++ characters.py
@@ -1,76 +1,81 @@
+import random
 
 
 class Character:
 
-    _hp: int
-    _attack: int
-    _hit_chance: int
-    _is_alive: bool = True
-
-    def get_hp(self):
-        return self._hp
-
-    def get_attack(self):
-        return self._attack
+    name: str
+    hp: int
+    attack: int
+    hit_chance: int
+    is_alive: bool = True
 
     def get_hit_chance(self):
-        return self._hit_chance
+        return self.hit_chance / 100
 
     def decrease_hp(self, damage: int):
-        self._hp -= damage
-        if self._hp <= 0:
-            self._hp = 0
-            self._is_alive = False
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0
+            self.is_alive = False
 
-    def get_is_alive(self):
-        return self._is_alive
+    def use_attack(self, victim_char: 'Character') -> int | None:
+        if random.random() < self.get_hit_chance():
+            damage = self.attack
+            victim_char.decrease_hp(damage=damage)
+            return damage
 
 
 class Penguin(Character):
 
-    _hp = 10
-    _attack = 2
-    _hit_chance = 90
+    name = 'Penguin'
+    hp = 10
+    attack = 2
+    hit_chance = 90
 
 
 class Cat(Character):
 
-    _hp = 12
-    _attack = 3
-    _hit_chance = 70
+    name = 'Cat'
+    hp = 12
+    attack = 3
+    hit_chance = 70
 
 
 class Granny(Character):
 
-    _hp = 12
-    _attack = 4
-    _hit_chance = 75
+    name = 'Granny'
+    hp = 12
+    attack = 4
+    hit_chance = 75
 
 
 class Teen(Character):
 
-    _hp = 20
-    _attack = 1
-    _hit_chance = 100
+    name = 'Teen'
+    hp = 20
+    attack = 1
+    hit_chance = 100
 
 
 class Soldier(Character):
 
-    _hp = 30
-    _attack = 8
-    _hit_chance = 80
+    name = 'Soldier'
+    hp = 30
+    attack = 8
+    hit_chance = 80
 
 
 class Stormtrooper(Character):
 
-    _hp = 25
-    _attack = 20
-    _hit_chance = 30
+    name = 'Stormtrooper'
+    hp = 25
+    attack = 20
+    hit_chance = 30
 
 
 class TRex(Character):
 
-    _hp = 100
-    _attack = 30
-    _hit_chance = 95
-
+    name = 'T-Rex Dinosaur'
+    hp = 100
+    attack = 30
+    hit_chance = 95
