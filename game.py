ce79ca8320e13a4c515b3465d66757922409af56
Iri O <oparina.iri@gmail.com>
2024-03-30 19:28:31 +0500

    Changed set_teams, team_move, end

--- game.py
+++ game.py
@@ -11,19 +11,23 @@ class Game:
     interface: Interface = Interface()
 
     def start(self):
+        self.interface.print_start()
         self.set_teams()
+        self.interface.print_teams(first_team=self.first_team, second_team=self.second_team)
         while True:
             self.first_team.update_moves()
             self.second_team.update_moves()
-            self.main_loop()
+            self._run()
 
     def set_teams(self):
-        cls, quantity = self.interface.get_character_class(), self.interface.get_character_quantity()
-        self.first_team = Team(char_class=cls, quantity=quantity)
-        cls, quantity = self.interface.get_character_class(), self.interface.get_character_quantity()
-        self.second_team = Team(char_class=cls, quantity=quantity)
+        char_class = self.interface.get_character_class(team_number=1)
+        quantity = self.interface.get_character_quantity(char_name=char_class.name)
+        self.first_team = Team(char_class=char_class, quantity=quantity)
+        char_class = self.interface.get_character_class(team_number=2)
+        quantity = self.interface.get_character_quantity(char_name=char_class.name)
+        self.second_team = Team(char_class=char_class, quantity=quantity)
 
-    def main_loop(self):
+    def _run(self):
         while self.first_team.moves > 0 or self.second_team.moves > 0:
             if self.first_team.moves > 0:
                 self.team_move(attacker=self.first_team, attacked=self.second_team)
@@ -31,16 +35,23 @@ class Game:
                 self.team_move(attacker=self.second_team, attacked=self.first_team)
 
     def team_move(self, attacker: Team, attacked: Team):
-        victim = random.choice(attacked.characters)
-        attacker.use_attack(victim_char=victim)
+        victim_char = random.choice(attacked.characters)
+        attacker_char = attacker.characters[0]
+        caused_damage = attacker_char.use_attack(victim_char=victim_char)
         attacked.update_alive()
-        if not victim.get_is_alive():
+        attacker.stats.update(damage=caused_damage)
+
+        if not victim_char.is_alive:
             self.interface.print_dead_character(attacker=attacker, attacked=attacked)
+            attacked.stats.update(is_victim_dead=True)
             attacked.moves -= 1
+
         if not attacked.characters:
             self.end(looser=attacked, winner=attacker)
+
         attacker.moves -= 1
 
     def end(self, looser: Team, winner: Team):
         self.interface.print_end(looser=looser, winner=winner)
+        self.interface.print_stats(first_team=self.first_team, second_team=self.second_team)
         sys.exit(0)
