f886e30d812d042fb6a94fc107d3500d3b4a2369
Iri O <oparina.iri@gmail.com>
2024-03-30 19:26:12 +0500

    Removed use_attack, created set_chars

--- team.py
+++ team.py
@@ -1,26 +1,22 @@
-import random
-from characters import Character
+from statistic import Statistic
 
 
 class Team:
 
     def __init__(self, char_class: type, quantity: int):
-        self.name = char_class.__name__
-        self.characters = [char_class() for _ in range(quantity)]
-        self.caused_damage = 0
-        self.quantity = quantity
+        self.name = char_class.name
+        self.characters = self.set_characters(
+            char_class=char_class,
+            quantity=quantity
+        )
+        self.stats = Statistic(survived=quantity)
         self.moves = quantity
 
+    def set_characters(self, char_class: type, quantity: int):
+        return [char_class() for _ in range(quantity)]
+
     def update_alive(self):
-        self.characters = list(filter(lambda char: char.get_is_alive(), self.characters))
+        self.characters = list(filter(lambda char: char.is_alive, self.characters))
 
-    def use_attack(self, victim_char: Character):
-        chance = self.characters[0].get_hit_chance()
-        chance /= 100
-        if random.random() < chance:
-            damage = self.characters[0].get_attack()
-            victim_char.decrease_hp(damage=damage)
-            self.caused_damage += damage
-
-    def update_moves(self):
-        self.moves = self.quantity
+    def update_moves(self):  # обновляет количество ходов
+        self.moves = len(self.characters)
