6637f380aba01fb894353c5523f8adfec4f2f29f
Iri O <oparina.iri@gmail.com>
2024-04-02 09:31:04 +0500

    Added docstring to set_chars and update_moves

--- team.py
+++ team.py
@@ -1,4 +1,3 @@
-from statistic import Statistic
 
 
 class Team:
@@ -9,14 +8,25 @@ class Team:
             char_class=char_class,
             quantity=quantity
         )
-        self.stats = Statistic(survived=quantity)
         self.moves = quantity
 
     def set_characters(self, char_class: type, quantity: int):
+        """
+        Создает список персонажей заданного класса.
+
+        :param char_class: Класс персонажа, который нужно создать
+        :param quantity: Количество персонажей, которые нужно создать
+        :return: Список персонажей заданного класса
+        """
         return [char_class() for _ in range(quantity)]
 
     def update_alive(self):
         self.characters = list(filter(lambda char: char.is_alive, self.characters))
 
-    def update_moves(self):  # обновляет количество ходов
+    def update_moves(self):
+        """
+        Обновляет количество ходов.
+
+        :return: None
+        """
         self.moves = len(self.characters)
